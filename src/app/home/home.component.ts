f1de2b277d385147ba63eed6cace4f8d6047d52c
Brejral <tcandrews32@gmail.com>
2019-07-12 07:05:37 -0700

    Updates

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from "@angular/core";
+import * as camera from 'nativescript-camera';
+import * as OCRPlugin from 'nativescript-ocr';
+
+var ocr = new OCRPlugin.OCR();
+
 
 @Component({
    selector: "Home",
@@ -8,11 +13,37 @@ import { Component, OnInit } from "@angular/core";
 })
 export class HomeComponent implements OnInit {
 
+   public receiptText: string = '';
+
    constructor() {
       // Use the component constructor to inject providers.
    }
 
    ngOnInit(): void {
       // Init your component properties here.
+   }
+
+   public onAddImage() {
+      camera.requestPermissions().then(
+         () => { // success
+            camera.takePicture().then(
+               (image) => {
+                  ocr.retrieveText({
+                     image
+                  }).then(
+                     (result) => {
+                        this.receiptText = result.text;
+                     }
+                  )
+               },
+               () => {
+
+               }
+            )
+         },
+         () => { // failure
+
+         }
+      );
    }
 }
